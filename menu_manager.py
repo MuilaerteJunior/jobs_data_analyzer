8ff0390bdde85d83676d7b6fdabfe2f3004b7fa0
Muilaerte Junior <mfvasjr@gmail.com>
2024-10-21 22:24:58 -0300

    Including a new feature to access job details through menu

--- menu_manager.py
+++ menu_manager.py
@@ -3,8 +3,9 @@ import os
 import threading
 import inquirer
 from collections import defaultdict
-from StatsMyData import StatsMyData
+from stats_data import StatsMyData
 from typing import Dict, List, Tuple, Callable
+from functools import partial
 
 class Menu:
     def __init__(self) -> None:
@@ -17,8 +18,21 @@ class Menu:
             ('Show top 10 companies hiring graph', self.study.top10CompaniesHiring),
             ('Show top 10 locations hiring graph', self.study.top10LocationsHiring),
             ('Show top 10 positions hiring graph', self.study.top10PositionsHiring),
+            ('View jobs offer by company', self.list_jobs_options),
         ]
         return menu_options
+
+    def print_out(self, iteems):
+        print(iteems[['job_title','job_location','link']])
+
+    def list_jobs_options(self) -> List[Tuple[str, Callable]]:
+        companies = self.study.getCompanies()
+        submenu_options = []
+        for company in companies:            
+            my_tuple = (f'{company[0]} - Count({len(company[1])})', partial(self.print_out, iteems= company[1]))            
+            submenu_options.append(my_tuple)
+        submenu_action = self.prompt_user(submenu_options, "pick up a company")
+        submenu_action()
 
     def prompt_user(self, choices: list[str, Callable], message: str) -> str:
         questions = [
@@ -26,13 +40,12 @@ class Menu:
         ]
         return inquirer.prompt(questions)['selection']
     
-    def choose_style(self):
-        options = self.list_menu_options()
+    def choose_action(self, options):
         final_style_choice = ("Exit",exit)
         options.append(final_style_choice)
         selected_choice = ""
         while (selected_choice != final_style_choice):
-            selected_choice = self.prompt_user(options, "What do you wanna do?")
+            selected_choice = self.prompt_user(options, "What do you wanna do?")            
             if selected_choice == final_style_choice:
                 print("\nBye!")
                 exit()
@@ -41,6 +54,6 @@ class Menu:
 
     def open_menu(self):
         os.system('cls' if os.name == 'nt' else 'clear')
-        self.choose_style()
+        self.choose_action(self.list_menu_options())
         a = self.selected_style
         os.system('cls' if os.name == 'nt' else 'clear')
--- stats_data.py
+++ stats_data.py
@@ -2,6 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import json
 from collections import defaultdict
+from typing import Dict, List, Tuple, Callable
+
 
 class StatsMyData:
     def __init__(self, jsonfileName):
@@ -26,27 +28,36 @@ class StatsMyData:
         print(sorted)
 
     def top10CompaniesHiring(self):
-        job_offer_by_company = self.data['company'].value_counts().head(10)#top 10
-        job_offer_by_company.plot(kind='barh', title='Testing')
+        job_offer_by_company = self.data['company'].value_counts().head(10)
+        job_offer_by_company.plot(kind='barh', title='Top 10 companies hiring')
         plt.ylabel('Company')
         plt.xlabel('Offer job count')
         plt.show()
 
 
     def top10PositionsHiring(self):
-        jobs_title_by_company = self.data['job_title'].value_counts().head(10)#top 10
-        jobs_title_by_company.plot(kind='barh', title='Testing')
+        jobs_title_by_company = self.data['job_title'].value_counts().head(10)
+        jobs_title_by_company.plot(kind='barh', title='Top 10 positions hiring')
         plt.ylabel('Job Title')
         plt.xlabel('Offer job count')
         plt.show()
 
 
     def top10LocationsHiring(self):
-        job_location_count = self.data['job_location'].value_counts().head(10)#top 10
-        job_location_count.plot(kind='barh', title='Testing')
+        job_location_count = self.data['job_location'].value_counts().head(10)
+        job_location_count.plot(kind='barh', title='Top 10 locations hiring')
         plt.ylabel('Job Location')
         plt.xlabel('Offer job count')
         plt.show()
+
+
+    def print_out(self, jobList):
+        for job in jobList:
+            print(job)
+
+    def getCompanies(self) -> Dict:
+        companiesGroup = self.data.groupby("company")
+        return companiesGroup
 
     def read_file(self):
         with open(self.json_file, 'r') as file:
